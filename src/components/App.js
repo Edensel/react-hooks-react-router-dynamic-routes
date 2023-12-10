008f774b131756b1cbc56dda24b53e4596b6e004
DenselEsekon <densel.esekon@moringaschool.com>
2023-12-10 16:36:02 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,19 @@
-import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import MoviesPage from "./MoviesPage";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import NavBar from './NavBar';
+import MoviesPage from './MoviesPage';
 
 function App() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "A River Runs Through It" },
-    { id: 2, title: "Se7en" },
-    { id: 3, title: "Inception" }
-  ]);
-
   return (
-    <div>
+    <BrowserRouter>
       <NavBar />
-      <Switch>
         <Route path="/movies">
-          <MoviesPage movies={movies} />
+          <MoviesPage />
         </Route>
         <Route exact path="/">
           <div>Home</div>
         </Route>
-      </Switch>
-    </div>
+    </BrowserRouter>
   );
 }
 
